1ac2a821468aef6d2bb53c92cf0a5933cadd8a3b
Mateus Honorato <mateush.honorato@gmail.com>
2019-10-08 00:02:37 -0300

    test commit

--- App.js
+++ App.js
@@ -13,7 +13,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
+    alignItems: 'bottom',
     justifyContent: 'center',
   },
 });
